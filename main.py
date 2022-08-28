8bb92f4730319c7db06342c56f58e2fc64423de2
kotama7 <kotamatakanori2@gmail.com>
2022-08-28 22:11:45 +0900

    場所の作成ができた

--- main.py
+++ main.py
@@ -3,36 +3,37 @@ import pyautogui
 import chore
 import time
 
+scr_w,scr_h= pyautogui.size()
+data_dict = {
+    'corrider':['./data/corrider.txt','',[2*scr_w,2*scr_h],[],[]],
+    'class_room_A':['./data/class_room.txt','./img/map/class_room.png',[scr_w,scr_h],[],[]],
+
+}
+# 辞書の内容は[map_path,image_path,image_size,image_position,player_position,player_location] 
+
 def new_game(): #最初のスタート画面
     new_game_b.destroy()
     canvas.delete('BG_start')
-    path = 'text/introduction.txt'
-    narration(path,0,scr_h/2)
+    location_name = 'corrider'
+    player_loc, map, scroll_boo = set_up(location_name,root,canvas) 
+    condition = True
+    root.bind('<KeyPress>',action)
     root.mainloop()
 
-def start():
-    print('start')
+def action(e):
+    key = e.keysym
 
-def narration(path,x_pos,y_pos):
-    def pop(i,label):
-        try:
-            label.destroy()
-        except:
-            pass
-        label = tkinter.Label(root,text=narrative[i].strip(),font=('System',24))
-        label.place(x=x_pos,y=y_pos)
-        print(narrative[i])
-        if i != len(narrative)-1:
-            root.after(1000,pop,i+1,label)
-        else:
-            root.after(3000,start)
-        root.mainloop()
-    with open(path,'r',encoding='utf-8') as f:
-        narrative = f.readlines()
-    root.after(1000,pop,0,None)
+def set_up(location):    #場面転換
+    data = data_dict[location]
+    map = chore.roommaker(data[0])
+    map_img = chore.resize(data[1],data[2][0],data[2][1])
+    canvas.create_image(data[3][0],data[3][1],image=map_img,tag=location)
+    player_img = chore.resize('./img/player/front.png',scr_w/10,scr_h/10)    #随時変更の必要あり
+    canvas.create_image(data[4][0],data[4][1],image=player_img,tag='Player')
+    player_loc = data[5]
+    return player_loc, map
 
 
-scr_w,scr_h= pyautogui.size()
 chore.BGM('./music/BGM/bird.mp3')
 root = tkinter.Tk()
 root.geometry(f'{scr_w}x{scr_h}')
